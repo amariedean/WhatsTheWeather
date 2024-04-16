e1d98d30f202005f5aab9a953ecd77bdfeabd08a
amariedean <159481179+amariedean@users.noreply.github.com>
2024-04-15 21:18:29 -0400

    Streamlined HTML + Created JS Template
    Tweaked spacing, class names and IDs

--- src/script.js
+++ src/script.js
@@ -66,7 +66,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">Fri</div>
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+              class="weather-forecast-icon"
+            />
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature"><strong>00</strong></div>
+              <div class="weather-forecast-temperature">00</div>
+            </div>
+          </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Miami");
+displayForecast();
